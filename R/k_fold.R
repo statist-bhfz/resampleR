5d6f2087a1a594ac5d960a81f9ed862d0c12afee
Andrey Ogurtsov <ogurtsov.a.b@gmail.com>
2019-01-05 14:54:51 +0200

    add new function, improve docs

--- R/k_fold.R
+++ R/k_fold.R
@@ -29,7 +29,7 @@ create_folds <- function(y,
 }
 
 
-#' Resamples for K-fold cross-validation stratified by target variable.
+#' Resamples for K-fold cross-validation with stratification by target variable.
 #'
 #' Creates resamples for K-fold cross-validation stratified by target variable.
 #'
@@ -45,6 +45,8 @@ create_folds <- function(y,
 #' cv_base(as.data.table(iris), "Species")
 #'
 #' @details
+#' Numeric target: quantile binning is used for stratification.
+#' Character/categorical target: resampling performs within categories.
 #' \code{probs} can be a vector like \code{c(0, seq(0.99, 1, length.out = 10))}
 #' for target with very skewed distribution, e.g. for financial data with 99\% of 0's.
 #'
@@ -74,8 +76,65 @@ cv_base <- function(data,
     return(splits[, ..cn])
 }
 
+#' Resamples for group K-fold cross-validation with stratification by target variable.
+#'
+#' @param data data.table with \code{y} and \code{id}.
+#' @param y Target variable name (character).
+#' @param id Identifier of each time series (character).
+#' @param nfolds Number of folds (min 2, max 20).
+#' @param probs Numeric vector of probabilities with values in \emph{[0, 1]} range.
+#'
+#' @return data.table with \code{nfolds} columns. Each column is an indicator variable
+#' with 1 corresponds to observations in validation dataset (stratified by target).
+#'
+#'@examples
+#' dt <- data.table(
+#'     user = rep(1:100, each = 5),
+#'     target = rnorm(5e2)
+#' )
+#' cv_split_group_kfold(dt, "target", "user")
+#'
+#' @details
+#' Numeric target: quantile binning is used for stratification.
+#'
+#' Character/categorical target: resampling performs within categories.
+#'
+#' \code{probs} can be a vector like \code{c(0, seq(0.99, 1, length.out = 10))}
+#' for target with very skewed distribution, e.g. for financial data with 99\% of 0's.
+#'
+#' Ensures that all observations for each \code{id} will be placed in the same fold.
+#' @export
+cv_split_group_kfold <- function(data,
+                                 y,
+                                 id,
+                                 nfolds = 5L,
+                                 probs = seq(0, 1, length.out = 11)) {
 
-#' Special resample strategy for K-fold cross-validation on time series data.
+    cols <- c(y, id)
+    assert_data_table(data)
+    assert_names(names(data), must.include = cols)
+    assert_integerish(nfolds, lower = 2L, upper = 20L)
+
+    data <- data[, .SD, .SDcols = cols]
+    splits <- data[, .(target = mean(get(y))), by = id]
+    splits[, fold := create_folds(y = get(y), nfolds = nfolds, probs = probs)]
+    splits <- splits[match(data[, get(id)], splits[, get(id)])]
+
+    cn <- paste0("split_", seq_len(nfolds))
+
+    # One hot for each split
+    for (i in seq_along(1:nfolds)) {
+        splits[, paste0("split_", i) := 0L]
+        splits[fold == i, paste0("split_", i) := 1L]
+    }
+
+    return(splits[, ..cn])
+
+}
+
+
+#' Special resampling strategy for K-fold cross-validation on time series data
+#' with stratification by target variable.
 #'
 #' @param data data.table with \code{y}, \code{id} and \code{time}.
 #' @param y Target variable name (character).
@@ -89,17 +148,25 @@ cv_base <- function(data,
 #'
 #' @examples
 #' dt <- data.table(
-#'     user = rep(1:100, each = 10),
-#'     date = as.POSIXct(rep(seq(1.8*10e8, 1.8*10e8 + 777600, by = 86400), 100),
+#'     user = rep(1:100, each = 5),
+#'     date = as.POSIXct(rep(seq(1.8*10e8, 1.8*10e8 + 388800, by = 86400), 100),
 #'                       origin = "1960-01-01"),
-#'     target = rnorm(1e3)
+#'     target = rnorm(5e2)
 #' )
 #' cv_split_temporal(dt, "target", "user", "date")
 #'
 #' @details
+#' Numeric target: quantile binning is used for stratification.
+#'
+#' Character/categorical target: resampling performs within categories.
+#'
+#' \code{probs} can be a vector like \code{c(0, seq(0.99, 1, length.out = 10))}
+#' for target with very skewed distribution, e.g. for financial data with 99\% of 0's.
+#'
 #' When some observations from one time series fall into validation fold, train/validation
 #' indices for this time series will be reassigned: only last observation will be in validation fold.
 #' This ensures that training performs on past data and predictions are made for future observations.
+#'
 #' TODO: allow to specify arbitrary number of observations for validation set.
 #'
 #' @export
@@ -130,8 +197,7 @@ cv_split_temporal <- function(data,
         splits[fold == i, paste0("split_", i) := 1L]
     }
 
-    to_replace <- splits[, .N, by = get(id)][N > 1]
-    setnames(to_replace, c(id, "N"))
+    to_replace <- splits[, .N, by = id][N > 1]
     f <- function(x) {
         if (any(x > 0L)) x <- c(rep(0L, length(x) - 1L), 1L)
         return(x)
@@ -140,47 +206,7 @@ cv_split_temporal <- function(data,
            (cn) := lapply(.SD, f),
            on = id,
            .SDcols = cn,
-           by = get(id)]
+           by = id]
 
     return(splits[, ..cn])
-}
-
-
-#' @title Функция генерирует таблицу с K разбивками на обучающую и
-#' проверочную выборки для K-fold кросс-валидации
-#'
-#' @param data Объект `data.table`.
-#' @param nfolds Количество фолдов.
-#' @param y Имена столбцов, участвующих в генерации фолдов.
-#' @param id Идентификатор наблюдений.
-#'
-#' @return Объект `data.table` с K разбивками на обучающую и
-#' проверочную выборки для K-fold кросс-валидации
-#' (со стратификацией по целевой переменной)
-#' @note
-#' Гарантирует, что наблюдения для одного пользователя всегда попадают в один фолд
-cv_split_group_kfold <- function(data,
-                                 nfolds = 5,
-                                 y = "totals.transactionRevenue",
-                                 id = "fullVisitorId") {
-    cols <- c(y, id)
-    assert_data_table(data)
-    assert_names(names(data), must.include = cols)
-    assert_integerish(nfolds, lower = 2L, upper = 20L)
-
-    data <- data[, .SD, .SDcols = cols]
-    splits <- data[, .(target = mean(get(y))), by = id]
-    splits[, fold := create_folds(target, nfolds)]
-    splits <- splits[match(data[, get(id)], splits[, get(id)])]
-
-    cn <- paste0("split_", seq_len(nfolds))
-
-    # One hot for each split
-    for (i in seq_along(1:nfolds)) {
-        splits[, paste0("split_", i) := 0L]
-        splits[fold == i, paste0("split_", i) := 1L]
-    }
-
-    return(splits[, ..cn])
-
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 resampleR
 =========
 
-**resamplerR** is like M.Kuhn\`s **rsample**, but all resamples are realised as plain data.table objects.This package aims to be as easy to extend as possible.
+**resamplerR** is like M.Kuhn\`s **rsample**, but all resamples are realised as plain data.table objects. This package aims to be as easy to extend as possible.
 
 Installation
 ------------
@@ -22,23 +22,53 @@ This is a basic example which shows you how to solve a common problem:
 ``` r
 library(resampleR)
 #> Loading required package: data.table
+
+cv_base(as.data.table(iris), "Species")
+#>      split_1 split_2 split_3 split_4 split_5
+#>   1:       1       0       0       0       0
+#>   2:       0       0       0       1       0
+#>   3:       1       0       0       0       0
+#>   4:       0       0       1       0       0
+#>   5:       0       1       0       0       0
+#>  ---                                        
+#> 146:       1       0       0       0       0
+#> 147:       1       0       0       0       0
+#> 148:       1       0       0       0       0
+#> 149:       0       0       0       1       0
+#> 150:       0       0       0       0       1
+
 dt <- data.table(
-    user = rep(1:100, each = 10),
-    date = as.POSIXct(rep(seq(1.8*10e8, 1.8*10e8 + 777600, by = 86400), 100),
+    user = rep(1:100, each = 5),
+    date = as.POSIXct(rep(seq(1.8*10e8, 1.8*10e8 + 388800, by = 86400), 100),
                       origin = "1960-01-01"),
-    target = rnorm(1e3)
+    target = rnorm(5e2)
 )
+
+cv_split_group_kfold(dt, "target", "user")
+#>      split_1 split_2 split_3 split_4 split_5
+#>   1:       1       0       0       0       0
+#>   2:       1       0       0       0       0
+#>   3:       1       0       0       0       0
+#>   4:       1       0       0       0       0
+#>   5:       1       0       0       0       0
+#>  ---                                        
+#> 496:       0       0       0       0       1
+#> 497:       0       0       0       0       1
+#> 498:       0       0       0       0       1
+#> 499:       0       0       0       0       1
+#> 500:       0       0       0       0       1
+
 cv_split_temporal(dt, "target", "user", "date")
-#>       split_1 split_2 split_3 split_4 split_5
-#>    1:       0       0       0       0       0
-#>    2:       0       0       0       0       0
-#>    3:       0       0       0       0       0
-#>    4:       0       0       0       0       0
-#>    5:       0       0       0       0       0
-#>   ---                                        
-#>  996:       0       0       0       0       0
-#>  997:       0       0       0       0       0
-#>  998:       0       0       0       0       0
-#>  999:       0       0       0       0       0
-#> 1000:       0       1       1       1       1
+#>      split_1 split_2 split_3 split_4 split_5
+#>   1:       0       0       0       0       0
+#>   2:       0       0       0       0       0
+#>   3:       0       0       0       0       0
+#>   4:       0       0       0       0       0
+#>   5:       1       1       1       0       1
+#>  ---                                        
+#> 496:       0       0       0       0       0
+#> 497:       0       0       0       0       0
+#> 498:       0       0       0       0       0
+#> 499:       0       0       0       0       0
+#> 500:       1       1       1       0       1
 ```
